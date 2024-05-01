7e2d9279e8a7ffce51d15c55208f1f7d43eb6d61
faris <535704264@qq.com>
2024-05-01 13:33:34 +0200

    add 子路由（嵌套页面切换，主页面不变）

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter)
 
 // 引入Home.vue, User.vue 组件
+import Main from '../views/Main.vue'
 import Home from '../views/Home.vue'
 import User from '../views/User.vue'
 
@@ -11,10 +12,19 @@ import User from '../views/User.vue'
 // 2.将路由和组件进行映射
 // 3.创建router，并对外暴露
 // 4.将router挂载到根节点（main.js）
+// 5.App.vue设置路由出口
 
 const routes = [
-    {path: '/home',component: Home},
-    {path: '/user',component: User}
+    // 主路由
+    {
+        path: '/',
+        component: Main,
+        children: [
+            // 子路由 （/可去掉）
+            {path: 'home',component: Home},
+            {path: 'user',component: User}
+        ]
+    }
 ]
 const route = new VueRouter({routes})
 export default route
---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      Main page.
+    </h1>
+    <!-- 子路由出口-->
+    <router-view></router-view>
+  </div>
+
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style>
+
+</style>
